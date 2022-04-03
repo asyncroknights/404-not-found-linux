f2823f73a6e43ea639e86e4dd24bea4a368f4f93
wriddhi <wriddhihazra@gmail.com>
2022-04-03 13:48:09 +0530

    All Dashboard Hooks Removed

--- pages/[user]/dashboard.js
+++ pages/[user]/dashboard.js
@@ -1,13 +1,8 @@
 import React from 'react'
 import Head from 'next/head'
-import { useRouter } from 'next/router'
 import Dashboard from '../../components/Dashboard'
 
 export default function dashboard() {
-
-    const router = useRouter()
-
-    const {user} = router.query
     return (
     <>
         <Head>
